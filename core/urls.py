ab4ab45c1b1e3b53d33904a8418bfb3a9efc3a87
lucas <lucashamacher01@gmail.com>
2026-04-08 11:05:24 +0200

    refactor: update import paths for core project

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for kanmind_backend project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("api/", include("auth_app.api.urls")),
+    path("api/", include("auth_app.api.urls")),
     # path("api/", include("boards_app.api.urls")),
     # path("api/", include("tasks_app.api.urls")),
 ]
