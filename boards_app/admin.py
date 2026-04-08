aa499516beed3d2f9f1e8ea66aa986300ca0c2c3
lucas <lucashamacher01@gmail.com>
2026-04-08 15:26:43 +0200

    add: board models

--- boards_app/admin.py
+++ boards_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from boards_app.models import Board
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner")
+    search_fields = ("title", "owner__email", "owner__fullname")
+    filter_horizontal = ("members",)
--- boards_app/models.py
+++ boards_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    members = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="boards",
+        blank=True,
+    )
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="owned_boards",
+    )
+    def __str__(self):
+        return self.title
