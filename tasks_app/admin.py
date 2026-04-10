1df5e1561aa4b0126ff091864854d3d726162a55
lucas <lucashamacher01@gmail.com>
2026-04-10 11:18:41 +0200

    add: admin rules for task_app

--- tasks_app/admin.py
+++ tasks_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks_app.models import Comment, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "board", "status", "priority", "due_date")
+    search_fields = ("title", "description")
+    list_filter = ("status", "priority", "due_date")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "task", "author", "created_at")
+    search_fields = ("content", "author__fullname", "task__title")
